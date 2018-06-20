3878393ae1ec0acdcc0488fa18a4945324ce50e3
Stephen Ciesla <s.ciesla@gmail.com>
2018-06-19 22:00:03 -0400

    Started adding mobs

--- Client/PSol.Data/Migrations/Configuration.cs
+++ Client/PSol.Data/Migrations/Configuration.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using PSol.Data.Models;
+
 namespace PSol.Data.Migrations
 {
     using System;
@@ -18,6 +21,36 @@ namespace PSol.Data.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+
+            context.Stars.AddOrUpdate(new Star
+            {
+                Id = "7179FC09-EBAA-49D8-8ED4-A53F7B95F260",
+                Name = "Vyt",
+                X = 4,
+                Y = 4,
+                Planets = new List<Planet>()
+            });
+
+            context.SaveChanges();
+
+            context.MobTypes.AddOrUpdate(new MobType
+            {
+                Id = "7A93241E-D251-4E7E-A565-46B0E274B5C6",
+                BonusExp = 0,
+                Credits = 0,
+                Level = 1,
+                MaxHealth = 100,
+                MaxShield = 0,
+                MaxSpawned = 4,
+                Name = "Vyt Scout",
+                SpawnRadius = 100,
+                SpawnTimeMax = 45,
+                SpawnTimeMin = 20,
+                Sprite = 2,
+                Star = context.Stars.FirstOrDefault(s => s.Name == "Vyt")
+            });
+
+            context.SaveChanges();
         }
     }
 }
---
+++ Client/PSol.Data/Models/Mob.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSol.Data.Models
+{
+    public class Mob
+    {
+        public string Id { get; set; }
+        public MobType Type { get; set; }
+        public float Health { get; set; }
+        public float Shield { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+        public int Dir { get; set; }
+        public float Rotation { get; set; }
+        public DateTime SpawnDate { get; set; }
+    }
+}
